d9505b4d586ee2c746fe84b233bdf8748b30ba01
Dheerajchandra <dheeraj.palusa@devrabbit.com>
2018-05-09 00:38:10 +0530

    first commit

---
+++ src/components/PageOne.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <span v-html="title"></span>
+    <h1>{{ name }}</h1>
+    <h2 v-text="firstname"></h2>
+    <ul v-if="showList" id="example-1">
+  <li v-for="item in items" :key="item">
+    {{ item.age }}
+  </li>
+</ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PageOne',
+  data () {
+    return {
+      name: 'Dheeraj started Vue.js',
+      title: '<h1>HEllo</h1>',
+      firstname: 'dheeraj',
+      showList: false,
+      items: [
+          {
+              roll: 2,
+              age: 22
+          },
+           {
+              roll: 3,
+              age: 23
+           }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import PageOne from '@/components/PageOne'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/PageOne',
+      name: 'PageOne',
+      component: PageOne
+    }
+  ]
+})
